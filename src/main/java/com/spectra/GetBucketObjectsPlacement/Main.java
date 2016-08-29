11a4a01ded7796288c4afef4eb1860a9caa6b694
Don Byington <donby@blacknblue.com>
2016-08-29 08:25:12 -0600

    Clearing house, starting fresh

--- src/main/java/com/spectra/GetBucketObjectsPlacement/Main.java
+++ src/main/java/com/spectra/GetBucketObjectsPlacement/Main.java
@@ -2,50 +2,43 @@ package com.spectra.GetBucketObjectsPlacement;
 
 import com.spectralogic.ds3client.Ds3Client;
 import com.spectralogic.ds3client.Ds3ClientBuilder;
+import com.spectralogic.ds3client.commands.GetBucketRequest;
+import com.spectralogic.ds3client.commands.GetServiceRequest;
+import com.spectralogic.ds3client.commands.GetServiceResponse;
+import com.spectralogic.ds3client.commands.spectrads3.*;
 import com.spectralogic.ds3client.helpers.Ds3ClientHelpers;
-import com.spectralogic.ds3client.models.Contents;
+import com.spectralogic.ds3client.models.*;
 import com.spectralogic.ds3client.models.bulk.Ds3Object;
 
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.UUID;
 
 public class Main {
     public static void main(final String[] args) throws IOException {
 
         final Ds3Client ds3Client = Ds3ClientBuilder.fromEnv().withHttps(false).build();
-        final Ds3ClientHelpers clientHelpers = Ds3ClientHelpers.wrap(ds3Client);
 
-        System.out.println("Getting local file list");
-        final Iterable<Ds3Object> localFiles = clientHelpers.listObjectsForDirectory(Paths.get(args[0]));
+        final GetBucketSpectraS3Response myBucket = ds3Client.getBucketSpectraS3(new GetBucketSpectraS3Request(args[0]));
+        final UUID myBucketUUID = myBucket.getBucketResult().getId();
 
-        System.out.println("Getting black pearl file list");
-        final Iterable<Contents> bpfilelist = clientHelpers.listObjects(args[1]);
-        final Iterable<Ds3Object> bpDs3Objects = clientHelpers.toDs3Iterable(bpfilelist);
+        System.out.println(myBucket.getBucketResult().getId());
 
-        final Set<Ds3Object> localSet = convertToSet(localFiles);
+        System.out.println("Getting objects");
 
-        System.out.println("Comparing...");
-        difference(localSet, bpDs3Objects);
+        final GetObjectsWithFullDetailsSpectraS3Response bucketObjects = ds3Client.getObjectsWithFullDetailsSpectraS3(new GetObjectsWithFullDetailsSpectraS3Request().withBucketId(args[0]));
 
-        if (localSet.isEmpty()) {
-            System.out.println("No differences");
-        } else {
-            System.out.println("Found differences:");
-            localSet.forEach(ds3Object -> System.out.println("File: " + ds3Object.getName()));
-        }
-    }
 
-    private static void difference(final Set<Ds3Object> localSet, final Iterable<Ds3Object> bpDs3Objects) {
-        bpDs3Objects.forEach(localSet::remove);
-    }
 
-    private static Set<Ds3Object> convertToSet(final Iterable<Ds3Object> localFiles) {
-        final Set<Ds3Object> returnSet = new HashSet<>();
+        System.out.println("done");
 
-        localFiles.forEach(returnSet::add);
 
-        return returnSet;
+
+
     }
 }
+
+
