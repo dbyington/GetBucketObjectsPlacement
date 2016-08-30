cbcb1ce3cb777ffe9c7582120dae3d49ef5d9048
Don Byington <donby@blacknblue.com>
2016-08-30 14:38:37 -0600

    get bucket list then get physical placement for objects, working

--- src/main/java/com/spectra/GetBucketObjectsPlacement/Main.java
+++ src/main/java/com/spectra/GetBucketObjectsPlacement/Main.java
@@ -1,44 +1,55 @@
 package com.spectra.GetBucketObjectsPlacement;
 
+import com.google.common.collect.Lists;
 import com.spectralogic.ds3client.Ds3Client;
 import com.spectralogic.ds3client.Ds3ClientBuilder;
-import com.spectralogic.ds3client.commands.GetBucketRequest;
-import com.spectralogic.ds3client.commands.GetServiceRequest;
-import com.spectralogic.ds3client.commands.GetServiceResponse;
 import com.spectralogic.ds3client.commands.spectrads3.*;
 import com.spectralogic.ds3client.helpers.Ds3ClientHelpers;
 import com.spectralogic.ds3client.models.*;
 import com.spectralogic.ds3client.models.bulk.Ds3Object;
 
 import java.io.IOException;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
 
 public class Main {
+
+
     public static void main(final String[] args) throws IOException {
 
         final Ds3Client ds3Client = Ds3ClientBuilder.fromEnv().withHttps(false).build();
+        final Ds3ClientHelpers clientHelpers = Ds3ClientHelpers.wrap(ds3Client);
+
+        clientHelpers.ensureBucketExists(args[0]);
+        System.out.println("Getting black pearl file list");
+        final Iterable<Contents> bpfilelist = clientHelpers.listObjects(args[0]);
+        final Iterable<Ds3Object> bpDs3Objects = clientHelpers.toDs3Iterable(bpfilelist);
+
+        System.out.println(bpDs3Objects.toString().length());
+        final List<Ds3Object> objectList = Lists.newArrayList(bpDs3Objects);
 
         final GetBucketSpectraS3Response myBucket = ds3Client.getBucketSpectraS3(new GetBucketSpectraS3Request(args[0]));
-        final UUID myBucketUUID = myBucket.getBucketResult().getId();
-
-        System.out.println(myBucket.getBucketResult().getId());
-
-        System.out.println("Getting objects");
-
-        final GetObjectsWithFullDetailsSpectraS3Response bucketObjects = ds3Client.getObjectsWithFullDetailsSpectraS3(new GetObjectsWithFullDetailsSpectraS3Request().withBucketId(args[0]));
+        System.out.printf("Bucket name: %s\tBucket UUID: %s\n", myBucket.getBucketResult().getName(),myBucket.getBucketResult().getId());
+        System.out.printf("Getting %f objects\n", bpDs3Objects.toString().length());
 
 
+        final GetPhysicalPlacementForObjectsWithFullDetailsSpectraS3Response phyicalPlacementResponse = ds3Client.getPhysicalPlacementForObjectsWithFullDetailsSpectraS3(new GetPhysicalPlacementForObjectsWithFullDetailsSpectraS3Request(args[0], objectList));
 
+        for (BulkObject obj : phyicalPlacementResponse.getBulkObjectListResult().getObjects()) {
+            System.out.printf("Object: %s\tUUID: %s\tTapes:", obj.getName(), obj.getId());
+
+            for (Tape tape : obj.getPhysicalPlacement().getTapes()) {
+                System.out.printf(" %s",tape.getBarCode());
+            }
+            System.out.printf("\n");
+        }
         System.out.println("done");
 
 
 
 
     }
+
+
 }
 
 
